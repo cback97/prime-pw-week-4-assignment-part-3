60e9d59379e45b5f0e8601d6f0969f091faba7ff
Christian Back <cdirt27@gmail.com>
2021-03-21 23:50:08 -0500

    Per each function there are now operation testers

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,22 +10,24 @@ function addItem(item) {
     }
     return false;
 }
+console.log(addItem('onion'));
+console.log(addItem('tomato'));
+console.log(addItem('lettuce'));
+console.log(addItem('bacon'));
+console.log(addItem('pudding'));
+
 
 function listItems(array) {
-    for (let i = 0; i <= array.length - 1; i++) {
+    for (let i = 0; i <=array.length -1; i++) {
         console.log('item in basket:', array[i]);
     }
 }
+listItems(basket)  
+
 
 function empty(array) {
     array.length = 0;
     console.log('Basket is now showing', array);
 }
 
-addItem('onion')
-addItem('tomato')
-addItem('lettuce')
-addItem('bacon')
-addItem('pudding')
-listItems(basket)
 empty(basket)
