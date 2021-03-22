7306f10fb611880e47c556fabafad3c5d5b4fa27
Christian Back <cdirt27@gmail.com>
2021-03-21 23:28:08 -0500

    created three functions which add, list, and remove items in basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,29 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem (item, basket) {
-    
+function addItem(item) {
+    console.log('We are adding:', item);
+    if (basket.push(item)) {
+        return true;
+    }
+    return false;
 }
+
+function listItems(array) {
+    for (let i = 0; i <= array.length - 1; i++) {
+        console.log('item in basket:', array[i]);
+    }
+}
+
+function empty(array) {
+    array.length = 0;
+    console.log('Basket is now showing', array);
+}
+
+addItem('onion')
+addItem('tomato')
+addItem('lettuce')
+addItem('bacon')
+addItem('pudding')
+listItems(basket)
+empty(basket)
